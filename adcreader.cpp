af870af5ab4b605c2a7217e77fe96155d0c3f6d6
DavoudiDavoud <davoudi.davoud@yahoo.com>
2016-04-05 10:54:30 +0100

    Update adcreader.cpp

--- adcreader.cpp
+++ adcreader.cpp
@@ -1,77 +1,248 @@
-#ifndef ADCREADER
-#define ADCREADER
+#include "adcreader.h"
+#include <QDebug>
 
-#include <QThread>
-
-#include <stdint.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include "gpio-sysfs.h"
 #include <fcntl.h>
 #include <sys/ioctl.h>
 #include <linux/types.h>
 #include <linux/spi/spidev.h>
-// sets the clock for the AD converter
-#include "gz_clk.h"
+#include <assert.h>
 
-// This class reads continously from the AD7705
-// and stores the data in a ringbuffer
-// which can be read with getSample() and
-// checked if data is available with hasSample().
-//
-// The class needs to be started from the main
-// program as a thread:
-// adcreader->start();
-// which is then running until the function
-// adcreader->quit(); is called. 
-// Then the standard method
-// adcreader->wait(); needs to be called
-// so that this thread has time to terminate.
-// TODO: make ADC parameters configurable such as gain, sampling rate etc.
-class ADCreader : public QThread
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
+
+#define MAX_SAMPLES 65536
+
+ADCreader::ADCreader()
 {
- public:
-  ADCreader();
+  int ret = 0;
+
+  // set up ringbuffer
+  samples = new int[MAX_SAMPLES];
+  // pointer for incoming data
+  pIn = samples;
+  // pointer for outgoing data
+  pOut = samples;
+
+  // SPI constants
+  static const char *device = "/dev/spidev0.0";
+  mode = SPI_CPHA | SPI_CPOL;
+  bits = 8;
+  speed = 50000;
+  delay = 10;
+  drdy_GPIO = 22;
   
-  // ring buffer functions
-  int hasSample();
-  int getSample();
-
- public:
-  // thread functions
-  void quit();
-  void run();
-
- protected:
-  void writeReset(int fd);
-  void writeReg(int fd, uint8_t v);
-  uint8_t readReg(int fd);
-  int readData(int fd);
-
- private:
-  bool running;
-  void pabort(const char *s);
+  // open SPI device
+  fd = open(device, O_RDWR);
+  if (fd < 0)
+    pabort("can't open device");
   
-  // file descriptor on the SPI interface
-  int fd;
-
-  // file descriptor on the interrupt pin
-  int sysfs_fd;
+  /*
+   * spi mode
+   */
+  ret = ioctl(fd, SPI_IOC_WR_MODE, &mode);
+  if (ret == -1)
+    pabort("can't set spi mode");
   
-  // data collected
-  int *samples;
+  ret = ioctl(fd, SPI_IOC_RD_MODE, &mode);
+  if (ret == -1)
+    pabort("can't get spi mode");
+  
+  /*
+   * bits per word
+   */
+  ret = ioctl(fd, SPI_IOC_WR_BITS_PER_WORD, &bits);
+  if (ret == -1)
+    pabort("can't set bits per word");
+  
+  ret = ioctl(fd, SPI_IOC_RD_BITS_PER_WORD, &bits);
+  if (ret == -1)
+    pabort("can't get bits per word");
+  
+  /*
+   * max speed hz
+   */
+  ret = ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
+  if (ret == -1)
+    pabort("can't set max speed hz");
+  
+  ret = ioctl(fd, SPI_IOC_RD_MAX_SPEED_HZ, &speed);
+  if (ret == -1)
+    pabort("can't get max speed hz");
+  
+  fprintf(stderr, "spi mode: %d\n", mode);
+  fprintf(stderr, "bits per word: %d\n", bits);
+  fprintf(stderr, "max speed: %d Hz (%d KHz)\n", speed, speed/1000);
+  
+  // enable master clock for the AD
+  // divisor results in roughly 4.9MHz
+  // this also inits the general purpose IO
+  gz_clock_ena(GZ_CLK_5MHz,5);
+  
+  // enables sysfs entry for the GPIO pin
+  gpio_export(drdy_GPIO);
+  // set to input
+  gpio_set_dir(drdy_GPIO,0);
+  // set interrupt detection to falling edge
+  gpio_set_edge(drdy_GPIO,"falling");
+  // get a file descriptor for the GPIO pin
+  sysfs_fd = gpio_fd_open(drdy_GPIO);
+  
+  // resets the AD7705 so that it expects a write to the communication register
+  writeReset(fd);
+  
+  // tell the AD7705 that the next write will be to the clock register
+  writeReg(fd,0x20);
+  // write 00001100 : CLOCKDIV=1,CLK=1,expects 4.9152MHz input clock
+  writeReg(fd,0x0C);
+  
+  // tell the AD7705 that the next write will be the setup register
+  writeReg(fd,0x10);
+  // intiates a self calibration and then after that starts converting
+  writeReg(fd,0x40);
+}
 
-  // pointer to ringbuffer
-  int *pIn;
-  int *pOut;
+void ADCreader::writeReset(int fd)
+{
+        int ret;
+        uint8_t tx1[5] = {0xff,0xff,0xff,0xff,0xff};
+        uint8_t rx1[5] = {0};
+        struct spi_ioc_transfer tr;
+	tr.tx_buf = (unsigned long)tx1;
+	tr.rx_buf = (unsigned long)rx1;
+	tr.len = ARRAY_SIZE(tx1);
+	tr.delay_usecs = delay;
+	tr.speed_hz = speed;
+	tr.bits_per_word = bits;
 
-  // spi constants for the ioctrl calls
-  uint8_t mode;
-  uint8_t bits;
-  uint32_t speed;
-  uint16_t delay;
-  int drdy_GPIO;
+        ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
+        if (ret < 1)
+                pabort("can't send spi message");
+}
 
-};
 
-#endif
+void ADCreader::writeReg(int fd, uint8_t v)
+{
+  int ret;
+  uint8_t tx1[1];
+  tx1[0] = v;
+  uint8_t rx1[1] = {0};
+  struct spi_ioc_transfer tr;
+  tr.tx_buf = (unsigned long)tx1;
+  tr.rx_buf = (unsigned long)rx1;
+  tr.len = ARRAY_SIZE(tx1);
+  tr.delay_usecs = delay;
+  tr.speed_hz = speed;
+  tr.bits_per_word = bits;
+	
+        ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
+        if (ret < 1)
+                pabort("can't send spi message");
+
+}
+
+uint8_t ADCreader::readReg(int fd)
+{
+        int ret;
+        uint8_t tx1[1];
+        tx1[0] = 0;
+        uint8_t rx1[1] = {0};
+        struct spi_ioc_transfer tr;
+	tr.tx_buf = (unsigned long)tx1;
+	tr.rx_buf = (unsigned long)rx1;
+	tr.len = ARRAY_SIZE(tx1);
+	tr.delay_usecs = delay;
+	tr.speed_hz = speed;
+	tr.bits_per_word = 8;
+
+        ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
+        if (ret < 1)
+          pabort("can't send spi message");
+          
+        return rx1[0];
+}
+
+int ADCreader::readData(int fd)
+{
+        int ret;
+        uint8_t tx1[2] = {0,0};
+        uint8_t rx1[2] = {0,0};
+        struct spi_ioc_transfer tr;
+	tr.tx_buf = (unsigned long)tx1;
+	tr.rx_buf = (unsigned long)rx1;
+	tr.len = ARRAY_SIZE(tx1);
+	tr.delay_usecs = delay;
+	tr.speed_hz = speed;
+	tr.bits_per_word = 8;
+
+        ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
+        if (ret < 1)
+          pabort("can't send spi message");
+          
+        return (rx1[0]<<8)|(rx1[1]);
+}
+
+
+
+
+
+void ADCreader::run()
+{
+  running = true;
+
+  fprintf(stderr,"We are running!\n");
+
+  while (running) {
+    
+    // let's wait for data for max one second
+    int ret = gpio_poll(sysfs_fd,1000);
+    if (ret<1) {
+      fprintf(stderr,"Poll error %d\n",ret);
+    }
+	  
+    // tell the AD7705 to read the data register (16 bits)
+    writeReg(fd,0x38);
+
+    // read the data register by performing two 8 bit reads
+    int value = readData(fd)-0x8000;
+
+    *pIn = value;
+    if (pIn == (&samples[MAX_SAMPLES-1])) 
+      pIn = samples;
+    else
+      pIn++;
+    
+  }
+  close(fd);
+  gpio_fd_close(sysfs_fd);
+}
+
+
+int ADCreader::getSample()
+{
+  assert(pOut!=pIn);
+  int value = *pOut;
+  if (pOut == (&samples[MAX_SAMPLES-1])) 
+    pOut = samples;
+  else
+    pOut++;
+  return value;
+}
+
+
+int ADCreader::hasSample()
+{
+  return (pOut!=pIn);
+}
+  
+
+void ADCreader::quit()
+{
+	running = false;
+	exit(0);
+}
+
+void ADCreader::pabort(const char *s)
+{
+        perror(s);
+        abort();
+}
